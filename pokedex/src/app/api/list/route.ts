788a651c336abcea2c36eb76d972b41dfd27f5a5
Salty876 <abdoulayes@mymacewan.ca>
2025-04-11 20:19:53 -0600

    finished the internal api for the main menu

---
+++ pokedex/src/app/api/list/route.ts
@@ -0,0 +1,31 @@
+import { PokemonSmall } from '../../components/interfaces'
+
+export async function GET(request: Request) {
+    //fetch data from 
+    const data = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1500")
+    const list = await data.json()
+
+
+    let menu: PokemonSmall[] = []
+
+    for (let i in list.results){
+       
+
+        const info = await fetch(`https://pokeapi.co/api/v2/pokemon/${list.results[i].name}`)
+        const pokemon = await info.json()
+
+        let addition:PokemonSmall = {
+            // abilities: [],
+            name: pokemon.name,
+            sprite: pokemon.sprites.front_default
+        };
+
+
+        // console.log(list.results[i].name)
+        menu.push(addition)
+    }
+    return new Response(JSON.stringify(menu), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' }
+    });
+  }
